5f2ee44db66502ac5be9eaa805d2ff0123c6ea51
Olli Koskinen <olli.s.koskinen@helsinki.fi>
2023-11-21 18:50:34 +0200

    removed coverage dependcy from normal group and possibly fixed pty value for tasks

--- rhythm-game/tasks.py
+++ rhythm-game/tasks.py
@@ -1,21 +1,28 @@
 from invoke import task
 from subprocess import call
 from sys import platform
+import sys
+
+def pty_status():
+    if sys.platform != 'win32':
+        return True
+    else:
+        return False
 
 @task
 def start(ctx):
-    ctx.run("python src/index.py")
+    ctx.run("python src/index.py", pty=pty_status())
 
 @task
 def test(ctx):
-    ctx.run("pytest src")
+    ctx.run("pytest src", pty=pty_status())
 
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest")
+    ctx.run("coverage run --branch -m pytest", pty=pty_status())
 
 @task(coverage)
 def coverage_report(ctx):
-    ctx.run("coverage html")
+    ctx.run("coverage html", pty=pty_status())
     if platform != "win32":
         call(("xdg-open", "htmlcov/index.html"))
