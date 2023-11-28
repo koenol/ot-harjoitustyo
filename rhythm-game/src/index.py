eb5d568a6ecac1bd91b6aa1d50d9eb4d7f0c85de
Olli Koskinen <olli.s.koskinen@helsinki.fi>
2023-11-28 20:45:23 +0200

    fix db conn

--- rhythm-game/src/index.py
+++ rhythm-game/src/index.py
@@ -3,8 +3,7 @@ from database.init_database_conn import initialize_database
 
 
 def main():
-    #db conn temporarily disabled
-    #initialize_database()
+    initialize_database()
     MainView()
 
 if __name__ == "__main__":
