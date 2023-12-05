46581690cb82efb84717b4c88800f2605132142e
Olli Koskinen <olli.s.koskinen@helsinki.fi>
2023-12-05 21:46:45 +0200

    update tuntikirjanpito, fix pylint errors

--- rhythm-game/dokumentaatio/tuntikirjanpito.md
+++ rhythm-game/dokumentaatio/tuntikirjanpito.md
@@ -1,19 +1,25 @@
-| päivä  | aika  | mitä tein                                                |
-| :----: | :---- | :------------------------------------------------------- |
-| 14.11. | 3,5   | Vaatimusmäärittelyn alustava toteutus ja brainstormausta |
-| 21.11. | 1     | Python-kertausta                                         |
-| 21.11. | 1,5   | Pygame-libraryyn tutustumista                            |
-| 21.11. | 1,5   | Harjoitustyön viikottaiset vaatimukset                   |
-| 21.11. | 0,5   | Repon uudelleenjärjestys ja linkkien korjauksia          |
-| 21.11. | 0,5   | SQL kertausta ja yhteyden luonti                         |
-| 22.11. | 1     | Highscoren renderauksen testaus ja repon siivousta       |
-| 27.11. | 1     | pygame documentation                                     |
-| 28.11. | 1     | pygame documentation                                     |
-| 28.11. | 0,75  | refractored db connection                                |
-| 28.11. | 0,5   | refractored pygame init/main view                        |
-| 28.11. | 0,5   | uusi näkymä: game-view                                   |
-| 28.11. | 0,5   | added test banner with custom font                       |
-| 28.11. | 0,75  | lisätty taustamusiikki soundcloud.com game-view          |
-| 28.11. | 0,75  | fixing pylint errors                                     |
-| 28.11. | 0,5   | alustava pakkauskaavio                                   |
-|  yht   | 15,75 |                                                          |
+| päivä  | aika | mitä tein                                                |
+| :----: | :--- | :------------------------------------------------------- |
+| 14.11. | 3,5  | Vaatimusmäärittelyn alustava toteutus ja brainstormausta |
+| 21.11. | 1    | Python-kertausta                                         |
+| 21.11. | 1,5  | Pygame-libraryyn tutustumista                            |
+| 21.11. | 1,5  | Harjoitustyön viikottaiset vaatimukset                   |
+| 21.11. | 0,5  | Repon uudelleenjärjestys ja linkkien korjauksia          |
+| 21.11. | 0,5  | SQL kertausta ja yhteyden luonti                         |
+| 22.11. | 1    | Highscoren renderauksen testaus ja repon siivousta       |
+| 27.11. | 1    | pygame documentation                                     |
+| 28.11. | 1    | pygame documentation                                     |
+| 28.11. | 0,75 | refractored db connection                                |
+| 28.11. | 0,5  | refractored pygame init/main view                        |
+| 28.11. | 0,5  | uusi näkymä: game-view                                   |
+| 28.11. | 0,5  | added test banner with custom font                       |
+| 28.11. | 0,75 | lisätty taustamusiikki soundcloud.com game-view          |
+| 28.11. | 0,75 | fixing pylint errors                                     |
+| 28.11. | 0,5  | alustava pakkauskaavio                                   |
+| 4.12.  | 0,5  | pygame official dokumentaatio/tutorial lukemista         |
+| 5.12.  | 1    | pygame official dokumentaatio/tutorial lukemista         |
+| 5.12.  | 0,5  | gameview lisätty pelaaja-painikkeet                      |
+| 5.12.  | 0,75 | gameview lisätty sound_block ja satunnais-spawn          |
+| 5.12.  | 0,5  | gameview lisätty toiminallisuus pisteiden keräämiseksi   |
+| 5.12.  | 0,75 | fixing pylint errors                                     |
+|  yht   | 19   |                                                          |
--- rhythm-game/src/services/game_view.py
+++ rhythm-game/src/services/game_view.py
@@ -1,17 +1,16 @@
-import pygame
 import random
+import pygame
 
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+FPS = 60
+BUTTON_SIZE = 48
+BLOCK_SPEED = 5
 
 
 class GameView:
     def __init__(self):
         pygame.init()
-        WINDOW_WIDTH = 800
-        WINDOW_HEIGHT = 600
-        FPS = 60
-        BUTTON_SIZE = 48
-        SOUND_BLOCK_BUFFER_DISTANCE = 50
-        BLOCK_SPEED = 5
 
         clock = pygame.time.Clock()
         score = 0
@@ -36,42 +35,36 @@ class GameView:
             (WINDOW_WIDTH // 2 - 256 - BUTTON_SIZE),
             (WINDOW_WIDTH // 2 - BUTTON_SIZE),
             (WINDOW_WIDTH // 2 + 256 - BUTTON_SIZE),
-]
+        ]
         game_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Rytmipeli - Game")
-
         pygame.mixer.music.load("src/static/Reminiscent-Of-Spring.mp3")
         pygame.mixer.music.play(1, 0.0, 8000)
         pygame.mixer.music.set_volume(0.40)
-
         block_x = WINDOW_WIDTH // 2 - 256 - BUTTON_SIZE
         block_y = WINDOW_HEIGHT // 2
-
-        
         running = True
+        button1_rect = pygame.draw.rect(game_surface, (255, 0, 0), button1_coord)
+        button2_rect = pygame.draw.rect(game_surface, (0, 255, 0), button2_coord)
+        button3_rect = pygame.draw.rect(game_surface, (0, 0, 255), button3_coord)
+
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.mixer.music.stop()
                     running = False
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_a:
-                        if button1_rect.colliderect(block_x, block_y, BUTTON_SIZE + 32, BUTTON_SIZE):
-                            score += 1
-                            block_y = -100
-                            block_x = random.choice(x_spawn_coords)
-                    if event.key == pygame.K_s:
-                        if button2_rect.colliderect(block_x, block_y, BUTTON_SIZE + 32, BUTTON_SIZE):
-                            score += 1
-                            block_y = -100
-                            block_x = random.choice(x_spawn_coords)
-                    if event.key == pygame.K_d:
-                        if button3_rect.colliderect(block_x, block_y, BUTTON_SIZE + 32, BUTTON_SIZE):
-                            score += 1
-                            block_y = -100
-                            block_x = random.choice(x_spawn_coords)
-
-
+                    if (
+                        (event.key == pygame.K_a and button1_rect.colliderect(
+                            block_x, block_y, BUTTON_SIZE + 32, BUTTON_SIZE)) or
+                        (event.key == pygame.K_s and button2_rect.colliderect(
+                            block_x, block_y, BUTTON_SIZE + 32, BUTTON_SIZE)) or
+                        (event.key == pygame.K_d and button3_rect.colliderect(
+                            block_x, block_y, BUTTON_SIZE + 32, BUTTON_SIZE))
+                    ):
+                        score += 1
+                        block_y = -100
+                        block_x = random.choice(x_spawn_coords)
 
             game_surface.fill((246, 246, 246))
 
@@ -81,8 +74,9 @@ class GameView:
             else:
                 block_y += BLOCK_SPEED
 
-            pygame.draw.rect(game_surface, (0, 0, 0), (block_x, block_y, BUTTON_SIZE + 32, BUTTON_SIZE))
-
+            pygame.draw.rect(
+                game_surface, (0, 0, 0), (block_x, block_y, BUTTON_SIZE + 32, BUTTON_SIZE)
+                )
 
             button1_rect = pygame.draw.rect(game_surface, (255, 0, 0), button1_coord)
             button2_rect = pygame.draw.rect(game_surface, (0, 255, 0), button2_coord)
@@ -90,6 +84,5 @@ class GameView:
 
             score_text = score_font.render(f"Score: {str(score)}", True, (0, 0, 0), (246, 246, 246))
             game_surface.blit(score_text, score_rect)
-            
             clock.tick(FPS)
             pygame.display.update()
